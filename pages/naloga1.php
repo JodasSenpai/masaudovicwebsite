b85fa2d9e408f611f6403721492c12c2591bd795
JodasSenpai <ziga.udovic1997@gmail.com>
2025-12-07 18:09:48 +0100

    test menu

--- pages/naloga1.php
+++ pages/naloga1.php
@@ -1,22 +1,22 @@
 <?php	
 	//include_once $_SERVER["DOCUMENT_ROOT"].'/config/dbconnect.php';
 	include_once $_SERVER["DOCUMENT_ROOT"].'/config/settings.php';
-	include_once $_SERVER["DOCUMENT_ROOT"].'/config/logincheck.php';
 ?>
 <!doctype html>
 <html lang="sl-SI">
 <head>
-	<title>Pregled</title>
+	<title>Eksperiment 67-A</title>
 	<?php include_once $_SERVER["DOCUMENT_ROOT"].'/view/global/main-header.php'; ?>
 	
 	
-	<?= smart_js('/styles/js/pregled/pregled.js', 'defer') ?> 
+	<?= smart_js('/styles/js/naloga1/naloga1.js', 'defer') ?> 
 </head>
 <body>
 	<?php include_once $_SERVER["DOCUMENT_ROOT"].'/view/global/main-menu.php'; ?>
 	<div id="mainscreen">		
 		<?php
-			include_once $_SERVER["DOCUMENT_ROOT"].'/view/pregled/pregled.php';
+			#include_once $_SERVER["DOCUMENT_ROOT"].'/view/naloga1/naloga1.php';
+            echo "Eksperiment 67-A";
 			
 		?>
 	</div>
--- view/pregled/pregled.php
+++ view/pregled/pregled.php
@@ -1,5 +1,3 @@
-<?php
-include_once $_SERVER["DOCUMENT_ROOT"].'/config/logincheck.php';
-?>
+
 
 <p>Stran se bo uporabljala kot shramba za vse moje uspešne projekte </p>
